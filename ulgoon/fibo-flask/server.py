0adf49f305e9f73f5d5e9760c7cb2d9dd145a323
ulgoon <ulgoon89@gmail.com>
2021-02-19 16:02:01 +0900

    tip: Add instruction to /secret route

--- ulgoon/fibo-flask/server.py
+++ ulgoon/fibo-flask/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 
 
 app = Flask(__name__)
@@ -7,5 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/secret')
+def get_secret():
+    # get all the secrets from origin
+    pass
+    
 if __name__=='__main__':
     app.run(host='localhost', port=4040, debug=True)
