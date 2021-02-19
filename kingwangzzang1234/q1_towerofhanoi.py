f9e6945dab07f63de8c4c933e18587ab90ee34f5
kingwangzzang1234 <wychoi0408@naver.com>
2021-02-19 01:52:46 +0000

    fix: string formatting
    using variable i

--- kingwangzzang1234/q1_towerofhanoi.py
+++ kingwangzzang1234/q1_towerofhanoi.py
@@ -1,6 +1,6 @@
 def run_hanoi(num):
-    for _ in range(num):
-        print('disc moved to pole2')
+    for i in range(num):
+        print('disc moved to pole{}'.format(i))
 
 if __name__=='__main__':
     run_hanoi(10)
