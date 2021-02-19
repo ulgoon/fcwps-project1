50f743ea0ad4fcd824afd67d11f1169828db1603
ulgoon <ulgoon89@gmail.com>
2021-02-19 15:50:27 +0900

    feat: Start flask project

---
+++ ulgoon/fibo-flask/server.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'home'
+
+if __name__=='__main__':
+    app.run(host='localhost', port=4040, debug=True)
