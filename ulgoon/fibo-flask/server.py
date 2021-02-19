4caa6755dcc8cc95d30899464b409a592c8dc330
ulgoon <ulgoon89@gmail.com>
2021-02-20 00:05:17 +0900

    feat: complete render index, redirect user's input

--- ulgoon/fibo-flask/server.py
+++ ulgoon/fibo-flask/server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import request, render_template, redirect, url_for
 import requests
 
 
@@ -8,10 +9,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/secret')
-def get_secret():
+@app.route('/getfibo', methods=['POST'])
+def get_fibo():
+    if request.method=='POST':
+        num = request.form['num']
+        return redirect(url_for('show_result', num=num))
+
+@app.route('/result')
+def show_result():
+    num = request.args['num']
+    #base_uri = 'https://fibonachicken.herokuapp.com/?ajax=1&people='
+    #complete_uri = base_uri + str(num)
     # get all the secrets from origin
-    pass
-    
+    #return requests.get(complete_uri).text
+    return num
+
 if __name__=='__main__':
     app.run(host='localhost', port=4040, debug=True)
