16eeded22128ad7295d848a126a50047ad70e051
ulgoon <ulgoon89@gmail.com>
2021-02-21 22:55:46 +0900

    feat: Start fibonachicken
    get number from user and print for testing

--- ulgoon/fibonachicken.py
+++ ulgoon/fibonachicken.py
@@ -1,10 +1,13 @@
-# Decide how many need to have chicken properly
-print('fibonachicken!!')
+def get_number():
+    return int(input("자애로운 자여, 몇명이나 먹이려고 하는고? "))
 
-# get fibonacci sequence
+def calculate_chicken(num):
+    pass    
 
-
-# print it!
-
-# with secret
-
+if __name__=='__main__':
+    people_count = get_number()
+    # Decide how many need to have chicken properly
+    # get fibonacci sequence
+    # print it!
+    # with secret
+    print(people_count)
